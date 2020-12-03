dfdb7d56e3fec2d1411d6791f716f7ccb7f9d5d8
BearingBall <74752877+BearingBall@users.noreply.github.com>
2020-12-03 16:34:43 +0700

    add

--- ex1/process.c
+++ ex1/process.c
@@ -1,27 +1,81 @@
 #include <unistd.h>
 #include <sys/types.h>
 
-void printIdentifier()
+void printUndefined(int c)
 {
- printf("ID:\nUser Effective: %d\nGroup Effective: %d\nUser Real: %d\nGroup Real: %d\n", geteuid(), getegid(), getuid(), getgid()); 
+	printf("operation %c undefined\n", c);	
 }
+
+void printIdentifierUserAndGroup()
+{
+	printf("ID:\nUser Effective: %d\nGroup Effective: %d\nUser Real: %d\nGroup Real: %d\n", geteuid(), getegid(), getuid(), getgid()); 
+}
+
+void setProccessAsLedear()
+{
+	if (setsid() == 0)
+	{
+		printf("successful\n");
+	}
+	else
+	{
+		perror(errno);
+	}
+}
+
+void printIdentifierProccesses()
+{
+	printf("Proccess ID = %d\nParent-proccess ID = %d\nGroup-proccess ID = %d\n", getpid(), getppid(), getid()); 
+}
+
+void printUlimit()
+{
+	printf("Ulimit =%ld\n", ulimit(UL_GETFSIZE,0));
+}
+
+void setUlimit(char* tmp)
+{
+	long size = atol(arg);
+	if (size == 0)
+	{
+		printf("wrong ulimit value\n");
+	}
+	else
+	{
+		size = ulimit(UL_SETFSIZE, size);
+	}
+}
+
+void printCoreFileSize()
+{
+	
+}
+
 
 int main(int argc, char *argv[])
 {
-	if (argc == 1)
+		if (argc == 1)
+		{
+			printf("Put more than 0 arguments\n");
+			return 0;
+		}
+	int c;
+	while((c=getopt(argc,argv,"ispuU:cC:dvV:")) != -1)
 	{
-		printf("Put more than 0 arguments\n");
-		return 0;
+		switch (c)
+		{
+			case 'i': printIdentifierUserAndGroup();  break;
+			case 's': setProccessAsLedear(); break;
+     			case 'p': printIdentifierProccesses(); break;
+     			case 'u': printUlimit(); break;
+     			case 'U': setUlimit(optarg); break;
+			case 'c': printCoreFileSize(); break;
+     			case 'C': setCoreFileSize(); break;
+     			case 'd': printDirectory(); break;
+     			case 'v': printVariables(); break;
+     			case 'V': setVariable(); break;
+      			default:  printUndefined(c); break;
+		}
 	}
-int c;
-while((c=getopt(argc,argv,"ispuU:cC:dvV:")) != -1)
-{
-	switch (c)
-	{
-	case 'i': printIdentifier();  break;
-	case 's': 
-	
-	}
-}
-return 0;
+	return 0;
 }
