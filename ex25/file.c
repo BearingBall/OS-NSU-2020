cecb1211c5e34aa8afcfcb551259a64124b9a4f5
BearingBall <74752877+BearingBall@users.noreply.github.com>
2020-12-15 15:31:39 +0700

    1

--- ex25/file.c
+++ ex25/file.c
@@ -7,8 +7,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <errno.h>
-#define COMMAND_LENGTH 200
-
+#define STRING_SIZE 300
 
 
 int main(int argc, char *argv[])
@@ -17,6 +16,13 @@ int main(int argc, char *argv[])
 	{
 		printf("wrong argv\n");
 	}
+	int pipe_ends[2];
+	if (pipe(pipe_ends) == -1)
+    	{
+        	printf("pipe error\n");
+        	return -1;
+    	}
+
 	pid_t pid;
 	pid = fork();
 	if (pid == -1)
@@ -24,29 +30,40 @@ int main(int argc, char *argv[])
 		printf("fork -1\n");
 		return -1;
 	}
+	char str[STRING_SIZE];
 	if (pid == 0)
-	{
-		//execlp("cat","cat",argv[1],(char*)NULL);
-		char*  str = malloc(COMMAND_LENGTH*sizeof(char));
-		str[0] = 'c';
-		str[1] = 'a';
-		str[2] = 't';
-		str[3] = ' ';
-		str[4] = '\0';
-		str = strncat(str,argv[1]);
-		if (system(str))
-		{
-			free(str);
-			printf("cat error\n");
-		}
-		free(str);
+	{//child
+		close(pipe_ends[1]);
+        	if (read(pipe_ends[0], str, 100) == -1)
+        	{
+            		printf("read error");
+            		close(pipe_ends[0]);
+            		return -1;
+        	}
+        	printf("received from pipe  %s\n", str);
+        	for (int i = 0; i < strlen(str); i++)
+        	{
+            		str[i] = (char)toupper(str[i]);
+        	}
+        	printf("after toupper %s\n", str);
+        	close(pipe_ends[0]);
 	}
 	else
 	{
+		close(pipe_ends[0]);
+        	strcpy(str, argv[1]);
+        	if (write(pipe_ends[1], str, strlen(str) + 1) == -1)
+        	{
+            		printf("write error");
+            		close(pipe_ends[1]);
+            		return -1;
+        	}
+        	printf("sent to pipe %s\n", str);
+        	close(pipe_ends[1]);
+
 		int status;
 		 do
         	{
-            		//(*)The waitpid() system call : It suspends execution of the calling process until a child specified by pid argument has changed state.
             		pid_t killedChildPid = waitpid(pid, &status, 0);
             		if(killedChildPid == -1)
             		{
